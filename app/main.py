c17c8fdd5a50938e716208ef65782fa4a6f1c0c2
Anthony Herbert <anthony@prettyprinted.com>
2022-04-28 10:02:28 +0700

    added support for simple where clause

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import sys
 from dataclasses import dataclass
 
 import sqlparse 
-from sqlparse.sql import IdentifierList, Function, Identifier
 
+from sqlparse.sql import IdentifierList, Function, Identifier, Comparison, Where
 from .record_parser import parse_record
 from .varint_parser import parse_varint
 
@@ -12,6 +12,43 @@ from .varint_parser import parse_varint
 database_file_path = sys.argv[1]
 command = sys.argv[2]
 
+
+def get_tablename(tokens):
+    # returns the first instance of identifer after the from keyword
+
+    from_clause_reached = False 
+    for token in tokens:
+        if token.value == 'from':
+            from_clause_reached = True
+        if from_clause_reached:
+            if type(token) == Identifier:
+                return token.value
+
+def get_where_clause(tokens):
+    for token in tokens:
+        if type(token) == Where:
+            return token 
+
+    return None
+
+def get_comparison(tokens):
+    for token in tokens:
+        if type(token) == Comparison:
+            return token
+
+    return None
+
+def get_col_and_value(token):
+    return (token.left.value, token.right.value)
+
+def get_where_condition(tokens):
+    where_clause = get_where_clause(tokens)
+    if where_clause:
+        comparison = get_comparison(where_clause.tokens)
+        if comparison:
+            return get_col_and_value(comparison)
+
+    return None
 
 @dataclass(init=False)
 class PageHeader:
@@ -69,6 +106,18 @@ def generate_schema_rows(database_path):
 
         return sqlite_schema_rows
 
+def get_table_columns(table_name, schema_rows):
+    table_record = [record for record in sqlite_schema_rows if record['tbl_name'].decode() == table_name][0]
+    table_sql = table_record['sql'].decode()
+
+    open_paren = table_sql.find('(')
+    table_sql = table_sql[open_paren + 1:len(table_sql) - 1].strip().split(',')
+    columns = []
+    for column_def in table_sql:
+        column_name = column_def.strip().split()[0]
+        columns.append(column_name)
+    return table_record, columns
+
 if command == ".dbinfo":
     sqlite_schema_rows = generate_schema_rows(database_file_path)
    # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -84,22 +133,21 @@ elif command == ".tables":
             output += tbl_name + ' '
     print(output)
 elif command.startswith('select'):
+    # parse the sql command
+    # if select statement
+    # get the columns of the table
+    # get all the rows in the table
+    # check the exact nature of the query. is function count? or regular sql query?
+    # if count, return len of rows
+    # if no where clause, output results
+    # if where clause, then filter results
+
     sql_tokens = sqlparse.parse(command)[0].tokens
 
+    table = get_tablename(sql_tokens)
 
-
-    table = command.split()[-1]
     sqlite_schema_rows = generate_schema_rows(database_file_path)
-    table_record = [record for record in sqlite_schema_rows if record['tbl_name'].decode() == table][0]
-    table_sql = table_record['sql'].decode()
-
-    open_paren = table_sql.find('(')
-    table_sql = table_sql[open_paren + 1:len(table_sql) - 1].strip().split(',')
-    columns = []
-    for column_def in table_sql:
-        column_name = column_def.strip().split()[0]
-        columns.append(column_name)
-
+    table_record, columns = get_table_columns(table, sqlite_schema_rows)
     column_count = len(columns) 
 
     page_size = get_page_size(database_file_path)
@@ -110,6 +158,8 @@ elif command.startswith('select'):
         page_header = PageHeader.parse_from(database_file)
         database_file.seek(page_start + 8) # move to the cell pointer array on the current page
         cell_pointers = [int.from_bytes(database_file.read(2), "big") for _ in range(page_header.number_of_cells)]
+
+        where_condition = get_where_condition(sql_tokens)
 
         table_rows = []
         for cell_pointer in cell_pointers:
@@ -124,13 +174,22 @@ elif command.startswith('select'):
                     row_dict[column] = rowid
                 else:
                     row_dict[column] = record[i]
-            table_rows.append(row_dict)
+            if where_condition:
+                if where_condition[0] == 'id':
+                    if rowid == int(where_condition[1]):
+                        table_rows.append(row_dict)
+                else:
+                    if row_dict[where_condition[0]].decode() == where_condition[1].strip('"').strip("'"):
+                        table_rows.append(row_dict)
+            else:
+                table_rows.append(row_dict)
+
 
         identifiers = sql_tokens[2] 
 
         if type(identifiers) == Function:
             # count
-            print(len(cell_pointers))
+            print(len(table_rows))
         elif type(identifiers) == Identifier:
             for row in table_rows:
                 print(row[identifiers.value].decode())
@@ -153,3 +212,10 @@ elif command.startswith('select'):
                 print(output)
 else:
     print(f"Invalid command: {command}")
+
+# column name
+# test 
+# value
+
+
+# the first identifier after from
